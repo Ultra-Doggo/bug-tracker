62735536586b0b57fde597203e3d9cc84d1aff1a
Edward Evangelista <ted11697@gmail.com>
2021-01-27 14:30:14 -0500

    authorization logic added | now, when user creates a task, they must be signed in first

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,10 +2,15 @@ const express = require('express')
 const {getTasks, createTask} = require('../controllers/task')
 const {createTaskValidator} = require('../validation')
 const {requireLogin} = require('../controllers/userAuth')
+const {userById} = require('../controllers/user')
+
 
 const router = express.Router()
 
 router.get('/', requireLogin, getTasks)
-router.post('/create-task', createTaskValidator, createTask)
+router.post('/create-task', requireLogin, createTaskValidator, createTask)
+
+router.param("userId", userById)
+
 
 module.exports = router
--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const { register, login, logout } = require('../controllers/userAuth')
 const { registrationValidator } = require('../validation')
+const {userById} = require('../controllers/user')
 
 const router = express.Router()
 
 router.post('/register', registrationValidator, register)
 router.post('/login', login)
 router.get('/logout', logout)
+
+router.param("userId", userById)
 
 module.exports = router
