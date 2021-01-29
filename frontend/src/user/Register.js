86147ad8eb34b7a06a8ab979903db3e516a6c6d1
Edward Evangelista <ted11697@gmail.com>
2021-01-28 22:38:05 -0500

    react frontend | user registration logic - now making appropriate api call

--- frontend/src/user/Register.js
+++ frontend/src/user/Register.js
@@ -13,6 +13,38 @@ class Register extends Component {
         }
     }
 
+    handleChange = (field) => (event) => {
+        this.setState({[field]: event.target.value})
+    }
+
+    clickSubmit = (event) => {
+        event.preventDefault()
+        const {firstName, lastName, email, password} = this.state
+        const user = {
+            firstName,
+            lastName,
+            email,
+            password
+        }
+        // console.log(user)
+        this.register(user)
+    }
+
+    register = user => {
+        return fetch("http://localhost:8080/register", {
+            method: "POST",
+            headers: {
+                Accept:"application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+    }
+
     render() {
         return (
             <div className="container">
@@ -20,21 +52,48 @@ class Register extends Component {
                 <form>
                     <div className="form-group">
                         <label className="text-muted">First Name</label>
-                        <input type="text" className="form-control"></input>
+                        <input 
+                            onChange={this.handleChange("firstName")} 
+                            type="text" 
+                            className="form-control"
+                            value={this.state.firstName}
+                            >    
+                        </input>
                     </div>
                     <div className="form-group">
                         <label className="text-muted">Last Name</label>
-                        <input type="text" className="form-control"></input>
+                        <input 
+                            onChange={this.handleChange("lastName")} 
+                            type="text" 
+                            className="form-control"
+                            value={this.state.lastName}
+                            >
+                        </input>
                     </div>
                     <div className="form-group">
                         <label className="text-muted">Email Address</label>
-                        <input type="email" className="form-control"></input>
+                        <input 
+                            onChange={this.handleChange("email")} 
+                            type="email" 
+                            className="form-control"
+                            value={this.state.email}
+                            >
+                        </input>
                     </div>
                     <div className="form-group">
                         <label className="text-muted">Password</label>
-                        <input type="password" className="form-control"></input>
+                        <input 
+                            onChange={this.handleChange("password")} 
+                            type="password" 
+                            className="form-control"
+                            value={this.state.password}
+                            >
+                        </input>
                     </div>
-                    <button className="btn btn-raised btn-primary">
+                    <button
+                        onClick={this.clickSubmit} 
+                        className="btn btn-raised btn-primary"
+                    >
                         Submit
                     </button>
                 </form>
