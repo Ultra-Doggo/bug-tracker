73c0fe9bbc7d26925fab6e09528dc41f4a1d9c0f
Edward Evangelista <ted11697@gmail.com>
2021-01-28 23:04:59 -0500

    registration form refactoring - moved into own method, rather than inside of the render()

--- frontend/src/user/Register.js
+++ frontend/src/user/Register.js
@@ -61,70 +61,79 @@ class Register extends Component {
         .catch(err => console.log(err))
     }
 
+    registrationForm = (firstName, lastName, email, password) => {
+        return (
+            <form>
+                <div className="form-group">
+                    <label className="text-muted">First Name</label>
+                    <input 
+                        onChange={this.handleChange("firstName")} 
+                        type="text" 
+                        className="form-control"
+                        value={this.state.firstName}
+                        >    
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Last Name</label>
+                    <input 
+                        onChange={this.handleChange("lastName")} 
+                        type="text" 
+                        className="form-control"
+                        value={this.state.lastName}
+                        >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Email Address</label>
+                    <input 
+                        onChange={this.handleChange("email")} 
+                        type="email" 
+                        className="form-control"
+                        value={this.state.email}
+                        >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input 
+                        onChange={this.handleChange("password")} 
+                        type="password" 
+                        className="form-control"
+                        value={this.state.password}
+                        >
+                    </input>
+                </div>
+                <button
+                    onClick={this.clickSubmit} 
+                    className="btn btn-raised btn-primary"
+                >
+                    Submit
+                </button>
+            </form>
+        )
+    }
+
     render() {
+        const {firstName, lastName, email, password} = this.state
         return (
             <div className="container">
                 <h1 className="mt-5 mb-5">Sign Up</h1>
-                <form>
-                    <div className="form-group">
-                        <label className="text-muted">First Name</label>
-                        <input 
-                            onChange={this.handleChange("firstName")} 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.firstName}
-                            >    
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Last Name</label>
-                        <input 
-                            onChange={this.handleChange("lastName")} 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.lastName}
-                            >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Email Address</label>
-                        <input 
-                            onChange={this.handleChange("email")} 
-                            type="email" 
-                            className="form-control"
-                            value={this.state.email}
-                            >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <label className="text-muted">Password</label>
-                        <input 
-                            onChange={this.handleChange("password")} 
-                            type="password" 
-                            className="form-control"
-                            value={this.state.password}
-                            >
-                        </input>
-                    </div>
-                    <button
-                        onClick={this.clickSubmit} 
-                        className="btn btn-raised btn-primary"
+
+                {this.registrationForm(firstName, lastName, email, password)}
+                
+                <div
+                    className="alert alert-danger"
+                    style={{display: this.state.error ? "" : "none"}} 
                     >
-                        Submit
-                    </button>
-                    <div
-                        className="alert alert-danger"
-                        style={{display: this.state.error ? "" : "none"}} 
-                        >
-                            {this.state.error}
-                    </div>
-                    <div
-                        className="alert alert-success"
-                        style={{display: this.state.success ? "" : "none"}} 
-                        >
-                            Your account was created! Please login.
-                    </div>
-                </form>
+                        {this.state.error}
+                </div>
+                <div
+                    className="alert alert-success"
+                    style={{display: this.state.success ? "" : "none"}} 
+                    >
+                        Your account was created! Please login.
+                </div>
             </div>
         );
     }
