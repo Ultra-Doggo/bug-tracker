155dd9510413fed542452213bb40f93db1ffc09d
Edward Evangelista <ted11697@gmail.com>
2021-01-31 12:24:51 -0500

    update form for user settings, with pre-populated fields

--- frontend/src/user/EditUser.js
+++ frontend/src/user/EditUser.js
@@ -32,15 +32,88 @@ class EditUser extends Component {
         })
     }
 
+    handleChange = (field) => (event) => {
+        this.setState({[field]: event.target.value})
+    }
+
+    clickSubmit = (event) => {
+        event.preventDefault()
+        const {firstName, lastName, email, password} = this.state
+        const user = {
+            firstName,
+            lastName,
+            email,
+            password
+        }
+        console.log(user)
+        // TODO: send new data to the backend
+    }
+
     componentDidMount() {
         const userId = isAuthenticated().user._id
         this.init(userId)
     }
 
+    updateForm = (firstName, lastName, email, password) => {
+        return (
+            <form>
+                <div className="form-group">
+                    <label className="text-muted">First Name</label>
+                    <input 
+                        onChange={this.handleChange("firstName")} 
+                        type="text" 
+                        className="form-control"
+                        value={firstName}
+                        >    
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Last Name</label>
+                    <input 
+                        onChange={this.handleChange("lastName")} 
+                        type="text" 
+                        className="form-control"
+                        value={lastName}
+                        >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Email Address</label>
+                    <input 
+                        onChange={this.handleChange("email")} 
+                        type="email" 
+                        className="form-control"
+                        value={email}
+                        >
+                    </input>
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input 
+                        onChange={this.handleChange("password")} 
+                        type="password" 
+                        className="form-control"
+                        value={password}
+                        >
+                    </input>
+                </div>
+                <button
+                    onClick={this.clickSubmit} 
+                    className="btn btn-raised btn-primary"
+                >
+                    Update
+                </button>
+            </form>
+        )
+    }
+
     render() {
+        const { firstName, lastName, email, password } = this.state
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">User Settings</h2>
+
+                {this.updateForm(firstName, lastName, email, password)}
             </div>
         );
     }
