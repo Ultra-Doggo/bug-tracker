c7499d1ba17285b0e7523c5cc88c13473ab7bd8e
Edward Evangelista <ted11697@gmail.com>
2021-01-30 19:06:23 -0500

    requests now using .env variable for API instead of hardcoded address

--- frontend/src/auth/index.js
+++ frontend/src/auth/index.js
@@ -19,7 +19,7 @@ export const authenticate = (jwt, next) => {
 
 export const register = (user) => {
     return (
-        fetch("http://localhost:8080/register", {
+        fetch(`${process.env.REACT_APP_API_URL}/register`, {
         method: "POST",
         headers: {
             Accept:"application/json",
@@ -35,18 +35,20 @@ export const register = (user) => {
 }
 
 export const login = user => {
-    return fetch("http://localhost:8080/login", {
-        method: "POST",
-        headers: {
-            Accept:"application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => {
-        return response.json()
-    })
-    .catch(err => console.log(err))
+    return (
+        fetch(`${process.env.REACT_APP_API_URL}/login`, {
+            method: "POST",
+            headers: {
+                Accept:"application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err))
+    ) 
 }
 
 export const logout = (next) => {
@@ -55,7 +57,7 @@ export const logout = (next) => {
     }
     next()
     return (
-        fetch("http://localhost:8080/logout", {
+        fetch(`${process.env.REACT_APP_API_URL}/logout`, {
                 method: "GET",
             })
             .then(response => {
