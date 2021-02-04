bcc5a0e3df408f173062a9d39b204e036e4fe794
Edward Evangelista <ted11697@gmail.com>
2021-02-04 13:16:26 -0500

    removal of Tasks component from Home page

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Tasks from '../task/Tasks'
 
 const Home = () => (
     <div className="mb-5">
         <div className="jumbotron">
             <h2>Home</h2>
             <p className="lead">Welcome to React Frontend</p>
-        </div>
-        <div className="container">
-            <Tasks />
         </div>
     </div>
 
